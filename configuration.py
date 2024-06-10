057838de64956cf7041806d8749a76d45348bdb8
renerodg <96renerodriguez@gmail.com>
2024-06-10 06:08:44 -0400

    URL actualizada

--- configuration.py
+++ configuration.py
@@ -1,3 +1,3 @@
-URL_SERVICE = "https://cnt-61416fdf-28b3-4994-89bd-ee5392052d31.containerhub.tripleten-services.com"
+URL_SERVICE = "https://cnt-4362f976-1f88-4c52-96b1-730985efdaa4.containerhub.tripleten-services.com"
 CREATE_USER_PATH = "/api/v1/users/"
 KITS_PATH = "/api/v1/kits"
